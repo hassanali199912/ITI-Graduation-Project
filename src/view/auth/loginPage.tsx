5d607f24593f503e7173299d9ba8f8e0f41bfc20
hassanali199912 <adsststjntun@gmail.com>
2025-06-22 23:50:45 +0300

    add some explins files

--- src/view/auth/loginPage.tsx
+++ src/view/auth/loginPage.tsx
@@ -1,14 +1,18 @@
 
 
+import { useTranslation } from "react-i18next";
 import Loader from "../../shared/components/loaders/LoginLoader";
 import { lazy, Suspense } from "react";
 
-const Login = lazy(() => import("../../features/Login/index"));
+const Login = lazy(() => import("../../features/Auth/index"));
 
 
 const LoginPage = () => {
+    const { t } = useTranslation("login");
+
     return (
         <Suspense fallback={<Loader />}>
+            <h1>{t("title")}</h1>
             <Login />
         </Suspense>
     )
