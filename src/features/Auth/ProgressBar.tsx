42c65d2ef5111910c012359bcd8405398ea56055
Manar-202 <119630678+Manar-202@users.noreply.github.com>
2025-06-29 02:51:03 +0300

    all

---
+++ src/features/Auth/ProgressBar.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import Logo from '../Auth/Logo';
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height:12,
+  borderRadius: 5,
+  transform: 'scaleX(-1)',
+  boxShadow: '0 1px 3px rgba(0, 0, 0, 0.08)',
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor: '#F2F4F8',
+    // ...theme.applyStyles('dark', {
+    //   backgroundColor: theme.palette.grey[800],
+    // }),
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 5,
+    backgroundColor: '#DBA507',
+    ...theme.applyStyles('dark', {
+      backgroundColor: '#F2F4F8',
+    }),
+  },
+}));
+
+interface Props {
+  activeStep: number;
+}
+
+
+export default function CustomizedProgressBars({activeStep}: Props) {
+  let totalSteps:number = 4;
+  const progressValue = ( activeStep / totalSteps) * 100;
+  return (
+    <>
+    <div dir='rtl'>
+      <Logo/>
+      <BorderLinearProgress  variant="determinate" value={progressValue} />
+    </div>
+    </>
+  );
+}
--- src/features/Auth/RegisterLearner/Step4Intro.tsx
+++ src/features/Auth/RegisterLearner/Step4Intro.tsx
@@ -8,8 +8,9 @@ interface Props {
 
 const Step4Intro = ({ data, setData, onBack }: Props) => {
   return (
-    <div className="flex flex-col bg-white rounded-lg shadow-md p-8 w-full max-w-4xl">
-      <h2 className="text-xl font-bold mb-6 text-gray-700 text-right">نبذة تعريفية</h2>
+    <div className="flex flex-col bg-white p-8 w-full max-w-4xl">
+      <h2 className="text-2xl font-bold mb-2 text-[#000000] text-right">نبذة تعريفية</h2>
+      <p className="text-sm text-[#A3A3A3] mb-8 text-right">هدفها: بناء ملف تعريفي يبان للمدربين.</p>
 
       <form
         onSubmit={(e) => {
@@ -18,37 +19,108 @@ const Step4Intro = ({ data, setData, onBack }: Props) => {
         }}
         className="grid grid-cols-1 gap-4"
       >
-        <TextField
-          label="نبذة عنك - أخبرنا عنك، أهدافك، وماذا تفعل"
-          fullWidth
-          multiline
-          rows={4}
-          value={data.about}
-          onChange={(e) => setData({ ...data, about: e.target.value })}
-        />
+        <div className="flex flex-col">
+          <label className="text-sm font-medium text-gray-800 mb-1 text-right">
+            نبذه عنك
+          </label>
+          <TextField
+            variant="outlined"
+            fullWidth
+            multiline
+            rows={4}
+            placeholder="أخبرنا عنك، أهدافك، وماذا تفعل"
+            value={data.about}
+            onChange={(e) => setData({ ...data, about: e.target.value })}
+            InputProps={{
+              style: {
+                backgroundColor: "#F4F9FB",
+                borderRadius: 8,
+                border: "none", 
+              },
+              notched: false, 
+            }}
+            sx={{
+              "& .MuiOutlinedInput-notchedOutline": {
+                border: "none",
+              },
+            }}
+          />
+        </div>
 
-        <TextField
-          label="رابط GitHub أو LinkedIn (اختياري)"
-          fullWidth
-          value={data.github}
-          onChange={(e) => setData({ ...data, github: e.target.value })}
-        />
+        <div className="flex flex-col">
+          <label className="text-sm font-medium text-gray-800 mb-1 text-right">
+            رابط GitHub أو LinkedIn (اختياري)
+          </label>
+          <TextField
+            variant="outlined"
+            fullWidth
+            value={data.github}
+            placeholder="example/linkedin"
+            onChange={(e) => setData({ ...data, github: e.target.value })}
+            InputProps={{
+              style: {
+                backgroundColor: "#F4F9FB",
+                borderRadius: 8,
+                border: "none", 
+              },
+              notched: false, 
+            }}
+            sx={{
+              "& .MuiOutlinedInput-notchedOutline": {
+                border: "none",
+              },
+            }}
+          />
+        </div>
 
         <div className="flex justify-between mt-4">
           <button
             type="button"
             onClick={onBack}
-            className="bg-gray-300 text-gray-700 px-6 py-2 rounded hover:bg-gray-400"
+            className="flex items-center gap-2 text-[#0003C7] font-medium"
           >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+            </svg>
             رجوع
           </button>
+
           <button
             type="submit"
-            className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700"
+            className="flex items-center gap-2 bg-[#0003C7] text-white px-6 py-2 rounded hover:bg-blue-800"
           >
-            إنهاء
+            استمرار
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+            </svg>
           </button>
         </div>
+        <div className="flex justify-center mt-6">
+          <p className="text-base text-[#A3A3A3] text-center">
+            لديك حساب؟{" "}
+            <span
+              className="cursor-pointer text-[#A3A3A3] hover:text-[#0003C7] transition"
+              onClick={() => window.location.href = "/login"} 
+            >
+              سجل دخول
+            </span>
+          </p>
+        </div>
+
       </form>
     </div>
   );
