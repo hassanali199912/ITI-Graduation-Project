79646ef1e02fa2b3bad67e55eb212324c80370a5
hassanali199912 <adsststjntun@gmail.com>
2025-06-29 20:45:21 +0300

    last 3

--- src/view/home/HomePage.tsx
+++ src/view/home/HomePage.tsx
@@ -1,14 +1,23 @@
 
 
 
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 
 
 
 const HomePage = () => {
 
     return (
-        <Outlet />
+        <>
+            <ul className="flex justify-center gap-3 flex-row p-3">
+                <li><Link to={"/"}>home</Link></li>
+                <li><Link to={"/auth"}>auth</Link></li>
+                <li><Link to={"/auth/new"}>New</Link></li>
+                <li><Link to={"/auth/mentor"}>mentor</Link></li>
+                <li><Link to={"/auth/tranner"}>tranner</Link></li>
+            </ul>
+            <Outlet />
+        </>
     )
 }
 
