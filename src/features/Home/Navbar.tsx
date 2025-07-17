3554f1f4311e7566529b6fd039c91024856d1a87
Mostafa Mamdouh <mmabdallah801@gmail.com>
2025-07-17 21:57:39 +0200

    Update Navbar.tsx

--- src/features/Home/Navbar.tsx
+++ src/features/Home/Navbar.tsx
@@ -3,41 +3,6 @@ import { Link, useNavigate } from 'react-router-dom';
 const Navbar = () => {
   const navigate = useNavigate();
 
-<<<<<<< HEAD
- const id=localStorage.getItem("teacherId");
-  useEffect(() => {
-    const storedRole = localStorage.getItem("role");
-    setRole(storedRole);
-  }, []);
-
- 
-  useEffect(() => {
-  const storedRole = localStorage.getItem("role");
-  setRole(storedRole);
-
-
-  const handleRoleUpdate = () => {
-    const updatedRole = localStorage.getItem("role");
-    setRole(updatedRole);
-  };
-
-  window.addEventListener("roleUpdated", handleRoleUpdate);
-
-  return () => {
-    window.removeEventListener("roleUpdated", handleRoleUpdate);
-  };
-}, []);
-
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("role");
-    setRole(null);
-    navigate("/");
-  };
-
-=======
->>>>>>> 197fe260aef18e4512e1a43be388f0d0b1196255
   return (
     <header dir="rtl" className="bg-white shadow-md">
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
@@ -73,53 +38,6 @@ const Navbar = () => {
           </Link>
         </nav>
 
-<<<<<<< HEAD
-          {role === "teacher" && (
-            <>
-              <Link to="/dashboardMentor" className="hover:text-blue-500 transition border border-blue-300 rounded-5 p-2">لوحة المرشد</Link>
-              <Link to={`/mentors/${id}`} className="hover:text-blue-500 transition border border-blue-300 rounded-5 p-2">الصفحة الشخصية </Link>
-            </>
-          )}
-
-          {role === "admin" && (
-            <>
-              <Link to="/dashboardAdmin" className="hover:text-blue-500 transition border border-blue-300 rounded-5 p-2">لوحة الإدارة</Link>
-              <Link to="/dashboardMentor" className="hover:text-blue-500 transition border border-blue-300 rounded-5 p-2">لوحة المرشد</Link>
-            </>
-          )}
-
-          {role === "student" && (
-            <Link to="/mentors" className="hover:text-blue-500 transition border border-blue-300 rounded-5 p-2">المرشدين</Link>
-          )}
-        </div>
-
-        <div className="flex gap-4">
-          {role ? (
-            <button
-              onClick={handleLogout}
-              className="bg-blue-700 px-4 py-2 rounded hover:bg-blue-800 transition text-white"
-            >
-              تسجيل الخروج
-            </button>
-          ) : (
-            <>
-              <button
-                type="button"
-                className="border border-blue-800 text-blue-800 px-6 py-2 rounded-lg hover:bg-blue-800 hover:text-white transition duration-200"
-                onClick={() => navigate('/home')}
-              >
-                سجل الدخول
-              </button>
-              <button
-                type="button"
-                className="bg-blue-800 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition duration-200"
-                onClick={() => navigate('/createAccount')}
-              >
-                انشاء حساب
-              </button>
-            </>
-          )}
-=======
         {/* Buttons */}
         <div className="flex gap-3">
           <button
@@ -134,7 +52,6 @@ const Navbar = () => {
           >
             انشاء حساب
           </button>
->>>>>>> 197fe260aef18e4512e1a43be388f0d0b1196255
         </div>
       </div>
     </header>
