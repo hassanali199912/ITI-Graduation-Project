1960280ba7667f8b53e67c19a9c63d9833db1c48
Mostafa Mamdouh <mmabdallah801@gmail.com>
2025-07-21 04:04:13 +0200

    ..

--- src/features/Sessions/SessionCard.tsx
+++ src/features/Sessions/SessionCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { SessionResponse } from "../Auth/RegisterMentor/types";
 import { Button, Chip, Paper, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const statusColor = {
   "0": { label: "قيد الانتظار", color: "warning" },
@@ -21,31 +22,42 @@ const SessionCard = ({ session }: Props) => {
         <Typography variant="body2" color="text.secondary">
           الوقت: {new Date(session.requestedDateTime).toLocaleString("ar-EG")}
         </Typography>
-        <Chip
-          label={statusInfo.label}
-          
-          variant="outlined"
-          size="small"
-        />
+        <Chip label={statusInfo.label} variant="outlined" size="small" />
       </div>
 
       <div dir="rtl" className="text-right space-y-1">
         {/* <Typography><strong>اسم الطالب:</strong> {session.studentName}</Typography> */}
-        <Typography><strong>الوصف:</strong> {session.description || "لا يوجد وصف"}</Typography>
-        <Typography><strong>المادة:</strong> {session.subject}</Typography>
-        <Typography><strong>عدد النقاط:</strong> {session.pointsAmount}</Typography>
-        <Typography><strong>المدة المتوقعة:</strong> {session.estimatedDurationMinutes} دقيقة</Typography>
-        <Typography><strong>تاريخ القبول:</strong> {session.acceptedAt ? new Date(session.acceptedAt).toLocaleString("ar-EG") : "لم يتم القبول بعد"}</Typography>
+        <Typography>
+          <strong>الوصف:</strong> {session.description || "لا يوجد وصف"}
+        </Typography>
+        <Typography>
+          <strong>المادة:</strong> {session.subject}
+        </Typography>
+        <Typography>
+          <strong>عدد النقاط:</strong> {session.pointsAmount}
+        </Typography>
+        <Typography>
+          <strong>المدة المتوقعة:</strong> {session.estimatedDurationMinutes}{" "}
+          دقيقة
+        </Typography>
+        <Typography>
+          <strong>تاريخ القبول:</strong>{" "}
+          {session.acceptedAt
+            ? new Date(session.acceptedAt).toLocaleString("ar-EG")
+            : "لم يتم القبول بعد"}
+        </Typography>
       </div>
 
       {session.status === 1 && (
-        <Button
-          variant="contained"
-          fullWidth
-          className="!mt-3 !bg-blue-600 hover:!bg-blue-700"
-        >
-          الانضمام للجلسة
-        </Button>
+        <Link to="/chat">
+          <Button
+            variant="contained"
+            fullWidth
+            className="!mt-3 !bg-blue-600 hover:!bg-blue-700"
+          >
+            بدء محادثة مع المرشـد
+          </Button>
+        </Link>
       )}
     </Paper>
   );
